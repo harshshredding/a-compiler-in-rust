2110b686e291b2749ab5689d323445127ba50874
Harsh Verma <harshshredding@gmail.com>
2023-02-17 19:51:14 -0500

    progress towards assignment 2

--- src/lexical_analysis.rs
+++ src/lexical_analysis.rs
@@ -1,30 +1,72 @@
 use regex::{Captures, Regex};
 use std::fs;
 
-// paranthesis: [, ], {, }, (, )
-// operator: +, -, *, /, =, ==, <>, <, >, <=, >=
-// split: (split_atom)*
-// split_atom: operator | paranthesis | space
-// identifier: alpha
-// Regex: (identifier split)*
-// - token*
-
-enum TokenType {
-    Split,
-    Identifier,
+// ,, +, -, or, [, intLit, ], =, class, id, {, }, ;, (, ), floatLit, not, :, void, ., *, /, and,
+// isa, eq, geq, gt, leq, lt, neq, if, then, else, read, return, while, write, float, integer,
+// private, public, function, arrow, constructor, attribute, sr, localVar
+#[derive(PartialEq, Eq)]
+pub enum TokenType {
+    Comma, // ,
+    Plus, // +
+    Minus, // -
+    Or, // or
+    OpenSquareBracket, // [
+    IntLit, // intLit
+    CloseSquareBracket, // ]
+    EqualsSymbol, // =
+    Class, // class
+    Identifier, // id
+    OpenCurly, // {
+    CloseCurly, // }
+    SemiColon, // ;
+    OpenParenthesis, // (
+    CloseParenthesis, // )
+    FloatLit, // floatLit
+    Not, // not
+    Colon, // :
+    Void, // void
+    Period, // period
+    Asterix, // *
+    ForwardSlash, // /
+    And, // and
+    IsA, // isa
+    Eq, // eq
+    GreaterThanOrEq, // geq
+    Greater, // gt
+    LessThanOrEq, // leq
+    LessThan, // lt
+    NotEqual, // neq
+    If, // if
+    Then, // then
+    Else, // else
+    Read, // read
+    Return, // return
+    While, // while
+    Write, // write
+    Float, // float
+    Integer, // integer
+    Private, // private
+    Public, // public
+    Function, // function
+    Arrow, // arrow
+    Constructor, // constructor
+    Attribute, // attribute
+    Sr, // sr
+    LocalVar, // localVar
 }
 
-struct Token {
-    token_text: String,
+pub struct Token {
+    token_type: TokenType,
+    lexeme: String
 }
 
-struct Scanner {
+pub struct Scanner {
     source_text: String,
     current_location: usize,
 }
 
 impl Scanner {
-    fn from(input_source: String) -> Self {
+    pub fn from(input_source: String) -> Self {
         return Self {
             source_text: input_source,
             current_location: 0,
@@ -45,29 +87,33 @@ impl Scanner {
         } else if self.current_location == self.source_size() {
             return None;
         } else {
-            let concerned_slice = &self.source_text[self.current_location..];
-            for (i, char) in concerned_slice.chars().enumerate() {
-                if !char.is_alphanumeric() {
-                    let token_string = &concerned_slice[..i];
-                    if token_string.is_empty() {
-                        self.current_location += 1;
-                        return Some(char.to_string());
-                    } else {
-                        return Some(token_string.to_string());
-                    }
-                } else {
-                    self.current_location += 1;
+            let concerned_slice:String = (&self.source_text[self.current_location..]).into();
+            let token_candidates = vec![
+                get_float_string(concerned_slice.clone()),
+                get_identifier_string(concerned_slice.clone()),
+                get_operator_string(concerned_slice.clone()),
+                get_whitespaces_string(concerned_slice.clone()),
+                get_integer_string(concerned_slice.clone()),
+                get_punctuation_string(concerned_slice.clone()),
+                get_reserved_keyword_string(concerned_slice.clone()),
+            ];
+            let token_candidates: Vec<Option<String>> = token_candidates.into_iter().filter(|x| x.is_some()).collect();
+            if token_candidates.is_empty() {
+                panic!("Could not tokenize !!!");
+            }
+            let mut longest_string:String = String::from("");
+            for candidate in token_candidates {
+                let candidate_string = candidate.unwrap();
+                if candidate_string.len() > longest_string.len() {
+                    longest_string = candidate_string;
                 }
             }
-            if !concerned_slice.is_empty() {
-                return Some(concerned_slice.to_string());
-            } else {
-                return None;
-            }
+            self.current_location += longest_string.len();
+            return Some(longest_string); 
         }
     }
 
-    fn get_all_tokens(&mut self) -> Vec<String> {
+    pub fn get_all_tokens(&mut self) -> Vec<String> {
         let mut ret: Vec<String> = Vec::new();
         loop {
             match self.next_token() {
@@ -85,43 +131,88 @@ impl Scanner {
     }
 }
 
-pub fn get_identifier(source_code_string: String) -> Option<String> {
-    let regex_string = r"^([A-Za-z]\w*)\W";
+
+fn get_token_if_valid(lexeme_string: Option<String>, token_type: TokenType) -> Option<Token> {
+    if lexeme_string.is_some() {
+        return Some(Token {token_type: token_type, lexeme: lexeme_string.unwrap() })
+    } else {
+        return None;
+    }
+}
+
+#[allow(dead_code)]
+pub fn get_identifier_token(source_code_string: String) -> Option<Token> {
+    return get_token_if_valid(get_identifier_string(source_code_string), TokenType::Identifier);
+}
+
+#[allow(dead_code)]
+pub fn get_identifier_string(source_code_string: String) -> Option<String> {
+    let regex_string = r"^([A-Za-z]\w*)(\W|$)";
     return get_token_using_regex(regex_string.into(), source_code_string)
 }
 
-pub fn get_integer(source_code_string: String) -> Option<String> {
-    let regex_string = r"^(([1-9]\d*)|0)\W";
+
+#[allow(dead_code)]
+pub fn get_integer_token(source_code_string: String) -> Option<Token> {
+    return get_token_if_valid(get_integer_string(source_code_string), TokenType::Integer);
+}
+
+#[allow(dead_code)]
+pub fn get_integer_string(source_code_string: String) -> Option<String> {
+    let regex_string = r"^(([1-9]\d*)|0)(\W|$)";
     return get_token_using_regex(regex_string.into(), source_code_string)
 }
 
-pub fn get_float(source_code_string: String) -> Option<String> {
-    let regex_string = r"^(((([1-9]\d*)|0)\.((\d+[1-9])|0))(e[+-](([1-9]\d*)|0))?)\W";
+#[allow(dead_code)]
+pub fn get_float_token(source_code_string: String) -> Option<Token> {
+    return get_token_if_valid(get_integer_string(source_code_string), TokenType::Float);
+}
+
+#[allow(dead_code)]
+pub fn get_float_string(source_code_string: String) -> Option<String> {
+    let regex_string = r"^(((([1-9]\d*)|0)\.((\d+[1-9])|0))(e[+-](([1-9]\d*)|0))?)(\W|$)";
     return get_token_using_regex(regex_string.into(), source_code_string)
 }
 
-pub fn get_operator(source_code_string: String) -> Option<String> {
+#[allow(dead_code)]
+pub fn get_operator_token(source_code_string: String) -> Option<Token> {
+    let operator_token_string = get_operator_string(source_code_string);
+    if operator_token_string.is_none() {
+        return None
+    } else {
+        let token_string = operator_token_string.unwrap();
+        match &token_string {
+            "==" => return Some(Token { token_type: TokenType::Eq, lexeme: token_string.clone()})
+        }
+    }
+}
+
+#[allow(dead_code)]
+pub fn get_operator_string(source_code_string: String) -> Option<String> {
     let regex_string = r"^(==|<>|<=|>=|\+|-|\*|/|=|and|or|not)";
-    return get_token_using_regex(regex_string.into(), source_code_string)
+    return get_token_using_regex(regex_string.into(), source_code_string);
 }
 
-pub fn get_punctuation(source_code_string: String) -> Option<String> {
+
+#[allow(dead_code)]
+pub fn get_punctuation_string(source_code_string: String) -> Option<String> {
     let regex_string = r"^(::|=>|\(|\)|\{|\}|\[|\]|;|,|\.|:)";
     return get_token_using_regex(regex_string.into(), source_code_string)
 }
 
-pub fn get_reserved_keyword(source_code_string: String) -> Option<String> {
-    let regex_string = r"^(integer|float|void|class|self|isa|while|if|then|else|read|write|return|localvar|constructor|attribute|function|public|private)[^a-z]";
+#[allow(dead_code)]
+pub fn get_reserved_keyword_string(source_code_string: String) -> Option<String> {
+    let regex_string = r"^(integer|float|void|class|self|isa|while|if|then|else|read|write|return|localvar|constructor|attribute|function|public|private)([^a-z]|$)";
     return get_token_using_regex(regex_string.into(), source_code_string)
 }
 
-pub fn get_whitespace(source_code_string: String) -> Option<String> {
+#[allow(dead_code)]
+pub fn get_whitespaces_string(source_code_string: String) -> Option<String> {
     let regex_string = r"^(\s+)(\S|$)";
     return get_token_using_regex(regex_string.into(), source_code_string)
 }
 
 fn get_token_using_regex(regex: String, source_code_string: String) -> Option<String> {
-    let source_code_string = source_code_string.clone() + "\n";
     let compiled_regex_obj = Regex::new(&regex).unwrap();
     let captures = compiled_regex_obj.captures(&source_code_string);
     return get_string_from_captures(captures);
@@ -138,17 +229,20 @@ fn get_string_from_captures(captures_option: Option<Captures>) -> Option<String>
     }
 }
 
+#[allow(dead_code)]
 fn read_source_test_file() -> String {
     let file_path = "src/test_resources/test_code.txt".to_string();
     let file_content = fs::read_to_string(file_path).expect("Could not read contents of file");
     return file_content;
 }
 
+#[allow(dead_code)]
 fn split_string_by_whitespace(some_string: String) -> Vec<String> {
     let split_up_string = some_string.split(" ").map(|s| s.to_string()).collect();
     return split_up_string;
 }
 
+#[allow(dead_code)]
 fn scan() -> String {
     String::from("A token")
 }
@@ -182,11 +276,13 @@ fn test_splitting_string() {
 #[test]
 fn test_create_token() {
     let some_token = Token {
-        token_text: String::from("This is a token string"),
+        lexeme: String::from("+"),
+        token_type: TokenType::Plus
     };
+    assert!(some_token.token_type == TokenType::Plus);
     assert_eq!(
-        some_token.token_text,
-        String::from("This is a token string")
+        some_token.lexeme,
+        String::from("+")
     );
 }
 
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,14 @@ mod lexical_analysis;
 mod synthesis;
 mod tests_lexical_analysis;
 
+fn count(curr: u32) {
+    println!("{}", curr);
+    if curr < 10 {
+        count(curr + 1)
+    }
+}
+
 fn main() {
-    println!("hello from main!");
+    count(0);
 }
 
